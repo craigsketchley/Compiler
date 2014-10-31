1c297286f8de9cc0356744bd264fdbe52e39bac6
prickprat <ricky.ratnayake@gmail.com>
2014-10-31 21:08:02 +1100

    more analysis

--- src/genKillFramework/ConstantFoldingAnalysis.java
+++ src/genKillFramework/ConstantFoldingAnalysis.java
@@ -1,12 +1,19 @@
 package genKillFramework;
 
-import java.util.HashMap;
-import java.util.Map;
+import intermediateLanguage.BinOpInstruction;
+import intermediateLanguage.Instruction;
+import intermediateLanguage.LcInstruction;
+import intermediateLanguage.Register;
 
-import CFG.ControlFlowGraph;
-import CFG.Node;
-import IntermediateLanguage.*;
-import Lattice.Lattice;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import lattice.Lattice;
+import cfg.ControlFlowGraph;
+import cfg.Node;
 
 public class ConstantFoldingAnalysis extends DataFlowAnalysis<Map<Register, Lattice<Integer>>>
 {
@@ -33,8 +40,71 @@ public class ConstantFoldingAnalysis extends DataFlowAnalysis<Map<Register, Latt
 	@Override
 	public Map<Register, Lattice<Integer>> meet(Node n)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		/* For all incoming Data Flow information we need to merge 
+		 * the values mapped to the register
+		 */
+		Map<Register, Lattice<Integer>> result = new HashMap<Register, Lattice<Integer>>();
+		
+		//Set<Register> registerIntersection = new HashSet<Register>();
+		
+		//Merge the Lattice Maps from predecessors\
+		for(Node pred : n.getAllPredecessors())
+		{
+			Map<Register, Lattice<Integer>> predMap = out.get(pred);
+			//For all Registers in the predecessor merge the Lattice into result
+			for(Register k : predMap.keySet())
+			{
+				if(!result.containsKey(k) || result.get(k).isStateBottom())
+				{
+					//Implies Register is undefined in the result
+					Lattice<Integer> newVal = new Lattice<Integer>(Lattice.State.BOTTOM);
+					result.put(k, newVal.merge(predMap.get(k)));
+				}
+				else
+				{
+					//Register exists in the result map, we should monotonically merge
+					result.put(k, newVal.merge(predMap.get(k)));
+				}
+				
+			}
+			
+			
+		}
+		
+		
+		return result;
+		
+//		Iterator<Node> it = n.getAllPredecessors().iterator();
+//		if(!it.hasNext())
+//		{
+//			//no predecessors
+//			return result;
+//		}
+//		//start with everything from first node
+//		result.putAll(out.get(it.next()));
+//		registerIntersection.addAll(out.get(it.next()).keySet());
+//		//then intersect the rest
+//		while(it.hasNext())
+//		{
+//			registerIntersection.retainAll(out.get(it.next()).keySet());
+//		}
+//		
+//		//now we need to see if we know the values
+//		for(Register key : registerIntersection)
+//		{
+//			it = n.getAllPredecessors().iterator();
+//			Lattice<String> value = out.get(it.next()).get(key);
+//			while(it.hasNext())
+//			{
+//				value.merge(out.get(it.next()).get(key));
+//			}
+//			if(value.getState() == Lattice.State.KNOWN)
+//			{
+//				result.put(key, value); //TODO: should we always put this in? e.g. when TOP?
+//			}
+//		}
+//		
+//		return result;
 	}
 
 	@Override
