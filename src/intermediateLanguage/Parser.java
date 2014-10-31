be02567ef21b7b16a7ce8e268062136a8d7f97e1
Joe Godbehere <jgod5665@uni.sydney.edu.au>
2014-10-31 19:53:20 +1100

    Parser throws (less informative...) exceptions. Parser tests. Updated docs

--- src/intermediateLanguage/Parser.java
+++ src/intermediateLanguage/Parser.java
@@ -5,6 +5,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -66,6 +68,24 @@ public class Parser
 	}
 	
 	/**
+	 * Read the file at path into a string
+	 * @param path to the file to read
+	 * @return contents of the file
+	 */
+	public static String path2string(String path)
+	{
+		try
+		{
+			return new String(
+					Files.readAllBytes(Paths.get(path)),
+					Charset.defaultCharset());
+		} catch (IOException e)
+		{
+			throw new ParseException("File could not be read: " + path);
+		}
+	}
+	
+	/**
 	 * Static method that accepts a file path and either returns a Program object,
 	 * or null, or throws an exception.
 	 * @param path to a file
@@ -73,17 +93,7 @@ public class Parser
 	 */
 	public static Program parse(String path)
 	{
-		//Read the file at path into a string
-		String code = null;
-		try
-		{
-			code = new String(Files.readAllBytes(Paths.get(path)), Charset.defaultCharset());
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-			return null; //Error reading file
-		}
-		return parseString(code);
+		return parseString(path2string(path));
 	}
 	
 	/**
@@ -94,27 +104,34 @@ public class Parser
 	 */
 	public static Program parseString(String code)
 	{
-		Scanner scan = buildScanner(code);
-		
-		//The program must start with a (
-		scan.next("\\(");
-		Program program = new Program();
+		try
+		{
+			Scanner scan = buildScanner(code);
+			
+			//The program must start with a (
+			scan.next("\\(");
+			Program program = new Program();
+	
+			//For each (, parse in a function
+			while(scan.hasNext("\\("))
+			{
+				program.functions.add(parseFunction(scan));
+			}
+			
+			//The program must end with a )
+			scan.next("\\)");
+			if(scan.hasNext())
+			{
+				throw new ParseException("Data in file after end of Program");
+			}
+			
+			//All done!
+			return program;
 
-		//For each (, parse in a function
-		while(scan.hasNext("\\("))
-		{
-			program.functions.add(parseFunction(scan));
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
 		}
-		
-		//The program must end with a )
-		scan.next("\\)");
-		if(scan.hasNext())
-		{
-			throw new ParseException("Data in file after end of Program");
-		}
-		
-		//All done!
-		return program;
+		throw new ParseException("Syntax error scanning Program");
 	}
 	
 	/**
@@ -135,36 +152,42 @@ public class Parser
 	 */
 	public static Function parseFunction(Scanner scan)
 	{
-		
-		//The function must start with a (
-		scan.next("\\(");
-		
-		//There must be a function ID
-		String id = scan.next(ID);
-		
-		//There must be a (arglist)
-		scan.next("\\(");
-		ArrayList<String> args = new ArrayList<String>();
-		while(scan.hasNext(ID))
+		try
 		{
-			args.add(scan.next(ID));
+			//The function must start with a (
+			scan.next("\\(");
+			
+			//There must be a function ID
+			String id = scan.next(ID);
+			
+			//There must be a (arglist)
+			scan.next("\\(");
+			ArrayList<String> args = new ArrayList<String>();
+			while(scan.hasNext(ID))
+			{
+				args.add(scan.next(ID));
+			}
+			scan.next("\\)");
+	
+			//Create an empty function with the header discovered so far
+			Function function = new Function(id, args);
+			
+			//Parse all blocks, which must start with (
+			while(scan.hasNext("\\("))
+			{
+				function.blocks.add(parseBlock(scan));
+			}
+			
+			//The function must end with )
+			scan.next("\\)");
+	
+			//All done!
+			return function;
+			
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
 		}
-		scan.next("\\)");
-
-		//Create an empty function with the header discovered so far
-		Function function = new Function(id, args);
-		
-		//Parse all blocks, which must start with (
-		while(scan.hasNext("\\("))
-		{
-			function.blocks.add(parseBlock(scan));
-		}
-		
-		//The function must end with )
-		scan.next("\\)");
-
-		//All done!
-		return function;
+		throw new ParseException("Syntax error scanning Function");
 	}
 	
 	/**
@@ -185,32 +208,38 @@ public class Parser
 	 */
 	public static Block parseBlock(Scanner scan)
 	{
-		//The blocks must start with a (, then a number		
-		scan.next("\\(");
-		Block block = new Block(scan.nextInt());
-		
-		//Change the delimiter to ), so we can easily grab whole instructions
-		scan.useDelimiter("\\)");
-		
-		//For each instruction
-		while(scan.hasNext(INSTRUCTION))
+		try
 		{
-			//Scan the instruction using space separator
-			scan.useDelimiter(" ");
-			block.instructions.add(parseInstruction(scan));
+			//The blocks must start with a (, then a number		
+			scan.next("\\(");
+			Block block = new Block(scan.nextInt());
 			
-			//Set the delimiter back to ) again, so we can check for another instruction
+			//Change the delimiter to ), so we can easily grab whole instructions
 			scan.useDelimiter("\\)");
-		}
-		
-		//Done reading instructions, set the delimiter back to space
-		scan.useDelimiter(" ");
-		
-		//The block must end with a )
-		scan.next("\\)");
+			
+			//For each instruction
+			while(scan.hasNext(INSTRUCTION))
+			{
+				//Scan the instruction using space separator
+				scan.useDelimiter(" ");
+				block.instructions.add(parseInstruction(scan));
+				
+				//Set the delimiter back to ) again, so we can check for another instruction
+				scan.useDelimiter("\\)");
+			}
+			
+			//Done reading instructions, set the delimiter back to space
+			scan.useDelimiter(" ");
+			
+			//The block must end with a )
+			scan.next("\\)");
+	
+			return block;
 
-		return block;
-		
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
+		}
+		throw new ParseException("Syntax error scanning Block");
 	}
 	
 	/**
@@ -231,64 +260,103 @@ public class Parser
 	 */
 	public static Instruction parseInstruction(Scanner scan)
 	{
-		//The instruction must start with a (, then the type
-		scan.next("\\(");
-		String op = scan.next();
-		
-		//Build the appropriate instruction type
-		Instruction instr;
-		switch(op)
+		try
 		{
-		case "ret": instr = new RetInstruction(parseReg(scan)); break;
-		case "ld": instr = new LdInstruction(parseReg(scan), parseId(scan)); break;
-		case "lc": instr = new LcInstruction(parseReg(scan), scan.nextInt()); break;
-		case "st": instr = new StInstruction(parseId(scan), parseReg(scan)); break;
-		case "br": instr = new BrInstruction(parseReg(scan), scan.nextInt(), scan.nextInt()); break;
-		case "call":
-			Register dest = parseReg(scan);
-			String id = scan.next(ID);
-			//keep reading arguments until we reach the end of the statement )
-			ArrayList<Register> args = new ArrayList<Register>();
-			while(scan.hasNext(REG))
+			//The instruction must start with a (, then the type
+			scan.next("\\(");
+			String op = scan.next();
+			
+			//Build the appropriate instruction type
+			Instruction instr;
+			switch(op)
 			{
-				args.add(parseReg(scan));
+			case "ret": instr = new RetInstruction(parseReg(scan)); break;
+			case "ld": instr = new LdInstruction(parseReg(scan), parseId(scan)); break;
+			case "lc": instr = new LcInstruction(parseReg(scan), scan.nextInt()); break;
+			case "st": instr = new StInstruction(parseId(scan), parseReg(scan)); break;
+			case "br": instr = new BrInstruction(parseReg(scan), scan.nextInt(), scan.nextInt()); break;
+			case "call":
+				Register dest = parseReg(scan);
+				String id = scan.next(ID);
+				//keep reading arguments until we reach the end of the statement )
+				ArrayList<Register> args = new ArrayList<Register>();
+				while(scan.hasNext(REG))
+				{
+					args.add(parseReg(scan));
+				}
+				instr = new CallInstruction(dest, id, args); break;
+			default: instr = new BinOpInstruction(op, parseReg(scan), parseReg(scan), parseReg(scan));
+			//default: throw new ParseException("Instruction type not recognised: " + op);
 			}
-			instr = new CallInstruction(dest, id, args); break;
-		default: instr = new BinOpInstruction(op, parseReg(scan), parseReg(scan), parseReg(scan));
-		//default: throw new ParseException("Instruction type not recognised: " + op);
-		}
-		
-		//then read the trailing ), then return the instruction
-		scan.next("\\)");
+			
+			//then read the trailing ), then return the instruction
+			scan.next("\\)");
+			return instr;
 
-		return instr;
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
+		}
+		throw new ParseException("Syntax error scanning Instruction");
 	}
-	
+
 	/**
-	 * Parser.parseId
+	 * Alternative entry point into parseId(Scanner)
+	 * It just generates a scanner based on the given string of code
+	 * @param a String of code starting with an ID
+	 * @return an ID object (String)
+	 */
+	public static String parseId(String code)
+	{
+		return parseId(buildScanner(code));
+	}
+
+	/**
 	 * Given a scanner, return an ID using the next token
+	 * @param scan
+	 * @return an ID from the scanner
 	 */
 	public static String parseId(Scanner scan)
 	{
-		if(scan.hasNext(ID))
+		try
 		{
-			return scan.next(ID);
+			if(scan.hasNext(ID))
+			{
+				return scan.next(ID);
+			}
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
 		}
 		throw new ParseException("Syntax error scanning ID");
 	}
 
 	/**
-	 * Parser.parseId
-	 * Given a scanner, return a Register using the next token
+	 * Alternative entry point into parseReg(Scanner)
+	 * It just generates a scanner based on the given string of code
+	 * @param a String of code starting with a Register
+	 * @return a Register object
+	 */
+	public static Register parseReg(String code)
+	{
+		return parseReg(buildScanner(code));
+	}
+
+	/**
+	 * Given a scanner, return a register using the next token
+	 * @param scan
+	 * @return a register from the scanner
 	 */
 	public static Register parseReg(Scanner scan)
 	{
-		if(scan.hasNext(REG))
+		try
 		{
-			//Get the register string, then discard the leading r to get the int
-			return new Register(Integer.parseInt(scan.next(REG).substring(1)));
+			if(scan.hasNext(REG))
+			{
+				//Get the register string, then discard the leading r to get the int
+				return new Register(Integer.parseInt(scan.next(REG).substring(1)));
+			}
+		} catch(InputMismatchException e) { ;
+		} catch(NoSuchElementException e) { ;
 		}
 		throw new ParseException("Syntax error scanning REG");
 	}
-
 }
--- src/intermediateLanguage/ParserTest.java
+++ src/intermediateLanguage/ParserTest.java
@@ -1,10 +1,83 @@
 package intermediateLanguage;
 
 import static org.junit.Assert.*;
+import intermediateLanguage.Parser.ParseException;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import optimisation.Optimiser;
+import optimisation.RedundantLoadOptimisation;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-public class ParserTest {
+public class ParserTest
+{
+	@Test
+	public void testParseFile() {
+		Program input = Parser.parse("input/redundantLoadsAssignmentSpec");
+		String expected = Parser.path2string("input/redundantLoadsAssignmentSpec");
+		expected = expected.replace("\t", "    "); //spaces not tabs
+		assertEquals(expected, input.toString());
+	}
+	
+	@Test
+	public void testParseReg()
+	{
+		try {
+			Parser.parseReg("r0");
+			fail("r0 should not parse");
+		} catch(ParseException e) {;}
+		try {
+			Parser.parseReg("r-1");
+			fail("r-1 should not parse");
+		} catch(ParseException e) {;}
+		try {
+			Parser.parseReg("R1");
+			fail("R1 should not parse");
+		} catch(ParseException e) {;}
+		try {
+			Parser.parseReg("1");
+			fail("1 should not parse (as a register)");
+		} catch(ParseException e) {;}
+		
+		for(int i = 1; i <101; ++i) {
+			assertEquals(i, Parser.parseReg("r" + i).register);
+		}
+	}
+
+	@Test
+	public void testParseID()
+	{
+		try {
+			Parser.parseId("0abc");
+			fail("0abc should not parse");
+		} catch(ParseException e) {;}
+		try {
+			Parser.parseId("_a");
+			fail("_a should not parse");
+		} catch(ParseException e) {;}
+		assertEquals("abc", Parser.parseId("abc"));
+	}
+
+	@Test
+	public void testParseInstruction()
+	{
+		try {
+			Parser.parseInstruction("0abc");
+			fail("0abc should not parse");
+		} catch(ParseException e) {;}
+		try {
+			Parser.parseInstruction("_a");
+			fail("_a should not parse");
+		} catch(ParseException e) {;}
+		Instruction i = Parser.parseInstruction("(ld r1 n)");
+		assertEquals("(ld r1 n)", i.toString());
+	}
 
 	@Test
 	public void testPatterns() {
